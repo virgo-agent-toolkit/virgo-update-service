f2eee14e16c0a749a76bf5590092ee193162c2d2
Ryan Phillips <ryan.phillips@rackspace.com>
2014-01-29 17:38:09 -0600

    add deployment log from dreadnot

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,15 @@ upgradeApp.controller('MainController', function($scope, UpgradeService, $locati
 
   UpgradeService.getAvailableVersions().success(function(data) {
     $scope.versions = data;
+  }).error(function(err) {
+    $scope.message = err;
   });
 
   $scope.deploy = function() {
-    UpgradeService.deploy($scope.deployData.channel,
-                          $scope.deployData.version).success(function() {
-                            $location.path('/deploy');
-                          });
+    UpgradeService.deploy($scope.deployData.channel, $scope.deployData.version)
+      .success(function() {
+        $location.path('/deploy');
+      });
   };
 });
 
