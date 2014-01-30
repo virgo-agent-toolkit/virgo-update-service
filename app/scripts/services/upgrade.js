f099500b26bea35c1ba13040a86790401a1d2110
Ryan Phillips <ryan.phillips@rackspace.com>
2014-01-30 10:26:55 -0600

    clean(service): Cleanup the upgrade service

--- app/scripts/services/upgrade.js
+++ app/scripts/services/upgrade.js
@@ -1,12 +1,11 @@
 var module = angular.module('upgradeAppServices', []);
-var service = function($http) {
-  var s = {};
-  s.getAvailableVersions = function() {
-    return $http.get('/v1/available_versions');
+module.factory('UpgradeService', function($http) {
+  return {
+    getAvailableVersions: function() {
+      return $http.get('/v1/available_versions');
+    },
+    deploy: function(deployData) {
+      return $http.post('/v1/deploy', deployData);
+    }
   };
-  s.deploy = function(deployData) {
-    return $http.post('/v1/deploy', deployData);
-  };
-  return s;
-};
-module.factory('UpgradeService', service);
+});
